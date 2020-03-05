f2922a1a1ce3e6ff5342d56b16b0f37de3e9613c
Adam Priest <sepone229@gmail.com>
2020-03-05 11:21:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ end
 def create_an_array
   space = ["Sun", "Earth", "Mars", "Saturn"]
 end
-def add_element_to_end_of_aray(array, element)
+def add_element_to_end_of_array(array, element)
   array.push(element)
 end
 def add_element_to_start_of_array(array, element)
