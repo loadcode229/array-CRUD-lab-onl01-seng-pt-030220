97dcec4201eb4c51c75c411b87ab4db21a8581b7
Adam Priest <sepone229@gmail.com>
2020-03-05 11:37:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,15 +16,12 @@ end
 def remove_element_from_start_of_array(array)
   array.shift
 end
-
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
-
 def retrieve_first_element_from_array(array)
   
 end
-
 def retrieve_last_element_from_array(array)
   
 end
